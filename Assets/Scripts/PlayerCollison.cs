2bbf8c8f186f93c81ce90cbe0ba7e457011ea0fb
alcatraz1010 <tamastorok97@gmail.com>
2019-06-17 02:07:54 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollison : MonoBehaviour
+{
+    public PlayerMovement movement;
+ 
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "obsticle")
+        {
+            movement.enabled = false;
+       
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+
+}
